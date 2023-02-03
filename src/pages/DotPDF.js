f77be2d29bb80d33dd55a22ced32edc8e6ab1db0
Axel Soler <axel@goodsoftwaredev.com>
2023-02-03 18:09:53 -0300

    create mail logo

--- src/pages/DotPDF.js
+++ src/pages/DotPDF.js
@@ -3,6 +3,8 @@ import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
 import CompanyPDF2 from '../components/CompanyPDF/CompanyPDF2';
 import { IconContext } from "react-icons";
 import { HiDownload } from "react-icons/hi";
+import { FiMail } from "react-icons/fi";
+
 
 const DotPDF = () => {
   const location = useLocation();
@@ -12,12 +14,17 @@ const DotPDF = () => {
   return (
     <div style={{position:"relative"}}>
       <div className="loader">
-        <h1 className="generatePDFTitle">GENERATING PDF . . .</h1>
+        <h1>GENERATING PDF . . .</h1>
         <div className="spinner" />
       </div>
       <PDFViewer style={{width:"100%", height:"100vh", position:"absolute", left:"0", top:"0", marginTop: "-20px", zIndex: "101"}}>
         <CompanyPDF2 pdfData={submissionData} />
       </PDFViewer>
+        <div className="p-2 hidden xl:block fixed w-fit mr-36 mt-2 right-0 top-0 z-[102] text-white bg-[rgb(50,54,57)]">
+          <IconContext.Provider value={{ size: "1.5rem" }}>
+            <FiMail />
+          </IconContext.Provider>
+        </div>
       <PDFDownloadLink
         className="hidden xl:block fixed w-fit mr-24 mt-2 right-0 top-0 z-[102] text-white bg-[rgb(50,54,57)]"
         document={<CompanyPDF2 pdfData={submissionData} />}
